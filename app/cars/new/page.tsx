3b85dbe302d206b2edb6ec7c0d114fba6542a0c7
Your Username <ubaidullah2305@gmail.com>
2026-01-26 04:32:18 +0500

    Fix import typo: @tanstack/react-query

--- app/cars/new/page.tsx
+++ app/cars/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useMutation, useQueryClient } from '@tanstack:react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { carsApi, mediaApi } from '@/lib/api';
